7bcbe21b80231552a95a5fea1c8d200463c90a55
Saul Zamora <saul.zamora@tagwizz.com.mx>
2023-04-10 20:08:55 -0600

    feat(views):create layout and index view

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,52 @@
+@extends('users.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-center">
+                <h1>Reto Laravel BigSmart - UserCrud</h1>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('users.create') }}"> Crear nuevo usuario</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @elseif($message = Session::get('error'))
+        <div class="alert alert-danger">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th width="280px">Opciones</th>
+        </tr>
+        @foreach ($users as $key=>$user)
+            <tr>
+                <td>{{ $key + 1}}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>
+                    <form action="{{ route('users.destroy',$user->id) }}" method="POST">
+
+                        <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Ver</a>
+
+                        <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Editar</a>
+
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+
+    {!! $users->links() !!}
+
+@endsection
