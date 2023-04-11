ae4deb2bca670f68eb48838512f9b72b458aefc6
Saul Zamora <saul.zamora@tagwizz.com.mx>
2023-04-11 02:34:32 -0600

    feat(UserController):attach role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
@@ -18,7 +19,9 @@ class UserController extends Controller
 
     public function create()
     {
-        return view('users.create');
+        $roles = Role::all();
+
+        return view('users.create', compact('roles'));
 
     }
 
@@ -33,9 +36,12 @@ class UserController extends Controller
             $request->validate([
                 'name' => 'required|max:75',
                 'email' => 'unique:users|email|max:50',
+                'role' => 'required',
             ]);
 
-            $user = User::create($request->all());
+            $user = User::create($request->except('role'));
+
+            $user->roles()->attach([$request->get('role')]);
 
             return redirect('users')->with('success','Usuario creado correctamente');
         } catch (ValidationException $validationException) {
@@ -58,7 +64,9 @@ class UserController extends Controller
 
     public function edit(User $user)
     {
-        return view('users.edit',compact('user'));
+        $roles = Role::all();
+
+        return view('users.edit',compact('user', 'roles'));
 
     }
 
@@ -74,10 +82,14 @@ class UserController extends Controller
             $request->validate([
                 'name' => 'required|max:75',
                 'email' => 'unique:users|email|max:50',
+                'role' => 'required',
+
             ]);
 
 
-            $user->update($request->all());
+            $user->update($request->except('role'));
+            $user->roles()->attach([$request->get('role')]);
+
 
             return redirect('users')->with('success','Usuario actualizado correctamente');
         } catch (ValidationException $validationException) {
