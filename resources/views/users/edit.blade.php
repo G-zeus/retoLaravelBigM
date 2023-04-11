72482fb5ef30c6d68f18d3ee63391fc3f0ffa996
Saul Zamora <saul.zamora@tagwizz.com.mx>
2023-04-10 20:09:45 -0600

    feat(views):create edit view

---
+++ resources/views/users/edit.blade.php
@@ -0,0 +1,57 @@
+@extends('users.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Editar usuario</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('users.index') }}"> Atras</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('users.update',$user->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Name:</strong>
+                    <input type="text" name="name" value="{{ $user->name }}" class="form-control" placeholder="Nombre">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Detail:</strong>
+                    <input name="email" value="{{ $user->email }}" type="email" class="form-control" placeholder="Email">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+            </div>
+
+        </div>
+
+    </form>
+    <form action="{{ route('users.destroy',$user->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+
+        <button type="submit" class="btn btn-danger">Eliminar</button>
+    </form>
+@endsection
