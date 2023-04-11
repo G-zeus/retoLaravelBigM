fdeb370ac262038fda7941491c401df38ddca187
Saul Zamora <saul.zamora@tagwizz.com.mx>
2023-04-10 20:10:00 -0600

    feat(views):create create view

---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,48 @@
+@extends('users.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Agregar nuevo usuario</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('users.index') }}"> Regresar</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('users.store') }}" method="POST" >
+        @csrf
+
+        <div class="row">
+            <div class="col-xs-2 col-sm-2 col-md-2">
+                <div class="form-group">
+                    <strong>Nombre:</strong>
+                    <input type="text" name="name" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-5 col-sm-5 col-md-5">
+                <div class="form-group ">
+                    <strong>Email:</strong>
+                    <input class="form-control" name="email" type="email" placeholder="emali@xxx.com">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Crear</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
